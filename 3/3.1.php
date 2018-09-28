3ca36daec69627e843d1fc2a626d3611d3a87ed2
AlexandrD <alexandr_do@mail.ru>
2018-09-28 23:32:57 +0300

    added class Human

--- 3/3.1.php
+++ 3/3.1.php
@@ -1,5 +1,32 @@
 <?php 
 
+class Human
+{
+	private static $count = 0;
+	private $firstName;
+	private $lastName;
+	private $age; 
+
+	public function __construct ($firstName, $lastName, $age)
+	{
+
+		$this->firstName = $firstName;
+		$this->lastName = $lastName;
+		$this->age = $age;
+	}
+
+	public function __get($name)
+	{	
+		return $this->$name;
+	}
+
+
+	public function getFullName()
+	{
+		return $this->firstName . ' ' . $this->lastName;
+	}
+}
+
 
 
 
