57ea35f1ef40ab72d7b55fea4184d096e07b26ab
AlexandrD <alexandr_do@mail.ru>
2018-09-28 13:10:18 +0300

    fixed 1.3 test text

--- 1/1.3.php
+++ 1/1.3.php
@@ -1,13 +1,10 @@
 <?php
 // 1.3. Текст произвольной длины, если текст большей, чем 50 символов длины, то обрезать его до 47 символов и в конце поставить "..." Троеточие не должно идти после пробела
-
-$text ="Lorem ipsum                                         ";
-
-iif (strlen($text) > 50) {
+$text ="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, quae fugit iusto exercitationem excepturi laborum sed inventore amet a fugiat.";
+if (strlen($text) > 50) {
 	$text = substr($text, 0, 47);
 	if ($text[46] != ' ') {
 		$text = $text . "...";	
-
 	} else {
 // Учитывает случай множественных пробелов:
 		for ($i=strlen($text) - 1; $i >= 0 ; $i--) { 
@@ -19,6 +16,5 @@ iif (strlen($text) > 50) {
 			}	
 	}
 }
-
 echo $text . "<br>";
 ?>
