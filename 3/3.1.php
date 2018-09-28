4a18c374f3a882e4cb669914b1e720f9573ffd5c
AlexandrD <alexandr_do@mail.ru>
2018-09-28 23:34:20 +0300

    added class Student

--- 3/3.1.php
+++ 3/3.1.php
@@ -27,6 +27,68 @@ class Human
 	}
 }
 
+class Student extends Human
+{
+	private static $count = 0;
+
+	// const TYPE_OCHN = 1;
+	// const TYPE_ZAOCHN = 2;
+
+	private $course;
+	private $type;
+	private $marks = [];
+	// private $type = self:: TYPE_OCHN;
+
+	public function setCourse($course)
+	{
+		$this->course = $course;
+	}
+
+	public function getCourse()
+	{
+		return $this->course;
+	}
+
+	public function setType($type){
+		$this->type = $type;
+	}
+
+	public function getType()
+	{
+		return $this->type;
+	}
+
+	public function giveMark($value)
+	{
+		array_push($this->marks, $value);
+	}
+
+	public function getMarks()
+	{
+		print_r( $this->marks);
+	}
+}
+
+// ------------------------------------------
+		// TESTING
+
+//-------------------------------------------
+
+$human1 = new Human('Alexandr', 'Dobynda', 26);
+
+$student1 = new Student('Tolik', 'Ivanov', 20);
+
+$student1->setCourse(3);
+$student1->setType('ZAOCHN');
+$student1->giveMark(5);
+$student1->giveMark(2);
+$student1->giveMark(3);
+$student1->giveMark(4);
+
+echo $student1->getFullName() . '<br>';
+echo $student1->getType() . '<br>';
+echo $student1->getCourse(). '<br>';
+$student1->getMarks(). '<br>';
 
 
 
